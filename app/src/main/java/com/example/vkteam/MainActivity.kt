f94fa2573855eab1d3a280b2f337441a110d1c4f
sd <ddarikin123497@gmail.com>
2023-03-29 11:09:19 +0300

    Start

---
+++ app/src/main/java/com/example/vkteam/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.vkteam
+
+//https://polis-mail.ru.github.io/2019-android/
+//https://github.com/dtrounine/snowflakes_homework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
